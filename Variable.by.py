611607c5e0ed13e7dc8b84e863742e663a14caa2
IamBuuz <asanzhimitypov@gmail.com>
2024-09-15 01:23:40 +0900

    init

---
+++ Variable.by.py
@@ -0,0 +1,7 @@
+name = 'Курс: Python, всего задач:'
+homeworks = 12
+homework_time = 1.5
+New_name = ', затрачено часов:'
+Average_hw_time = ', среднее время выполнения'
+name1 = 'часa'
+print(name,homeworks,New_name,homework_time,Average_hw_time,homework_time/homeworks, name1)
