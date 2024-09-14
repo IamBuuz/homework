da165fffc6b36ff182044c289a9232541e8e8d88
IamBuuz <asanzhimitypov@gmail.com>
2024-09-15 01:21:51 +0900

    init

---
+++ variable1.py
@@ -0,0 +1,5 @@
+name = 'Python'
+hw = 12
+hw_time = 1.5
+average_hw_time = hw_time/hw
+print('Курс:', name, ', всего задач:', hw, ', затрачено часов:', hw_time, ', среднее время выполнения', average_hw_time, 'часа')
